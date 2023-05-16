45f38edf72b7f88ca2fe6581d8eea2f9e887828e
Janesse <jlian100@ucsc.edu>
2023-05-16 11:04:39 -0700

    lab 11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,26 +1,13 @@
-// index.js - working with partner to using Javascript arrays 
+// index.js - working with partner to using Javascript libraries & jQuery
 // Author: Janesse Liang <jlian100@ucsc.edu>
-// Date: May 3, 2023
+// Date: May 16, 2023
 
-// sortUserName - a function that takes user input and sorts the lettrs
-// of their name
-function sortUserName() {
-  var userName = window.prompt("Hi! Tell me your name, so I could fix it.")
-  console.log("userName =", userName);
-  // split string to array
-  var nameArray = userName.split('');
-  console.log("nameArray =", nameArray);
-  // sort the array
-  var nameArraySort = nameArray.sort();
-  console.log("nameArraySort =", nameArraySort);
-  // join array back to string
-  var nameSorted = nameArraySort.join('');
-  console.log("nameSorted =", nameSorted);
-  // note that above could be done in a single line:
-  // userName.toLower().split('').sort(). join('')
-  return nameSorted;
-}
+// created buttons in each minor section
 
-// Output
-document.writeln("Hey, I fixed your name: ",
-  sortUserName(), "</br>");
+buttonEl = $('.minor-section').append('<button id=button>click me</button>');
+
+// toggle class specail and alert on button click
+$(buttonEl).click(function () {
+  $('.minor-section').toggleClass('special');
+  alert("The button has been clicked!");
+});
